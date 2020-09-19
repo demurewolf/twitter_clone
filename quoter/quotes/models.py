1da7a2eddbd3e59962f4e98581af7589b83803bd
demurewolf <josiahwedgwood@ymail.com>
2020-09-19 04:19:25 -0400

    Added base model for quotes

--- quoter/quotes/models.py
+++ quoter/quotes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+    content = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    likes = models.IntegerField(default=0)
+    requotes = models.IntegerField(default=0)
+    
+    #  images = models.ImageField()
+    # replies
+     
