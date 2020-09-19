78f25f3ed0289fbfb5d582d9a7768d4d972e92d7
demurewolf <josiahwedgwood@ymail.com>
2020-09-19 04:19:39 -0400

    Added base model for users

--- quoter/users/models.py
+++ quoter/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    bio = models.CharField(max_length=250)
+    
+
+    # following
+    # followers
+    # pub_quotes
+    # likes
+    # re_quotes
