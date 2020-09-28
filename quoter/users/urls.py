819174949ac94d6ea7a6214bc59773dfff46dd90
demurewolf <josiahwedgwood@ymail.com>
2020-09-28 11:08:45 -0400

    Added search url

--- quoter/users/urls.py
+++ quoter/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
+    path('search/', views.search, name='search'),
     path('<slug:username>/', views.profile, name='profile'),
     path('<slug:username>/followers', views.followers, name='followers'),
     path('<slug:username>/following', views.following, name='followers'),
