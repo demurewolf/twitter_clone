a2181b919020880ba93040c666059cd3d69cde93
demurewolf <josiahwedgwood@ymail.com>
2020-12-01 17:36:48 -0500

    User can update their own profile!

--- quoter/users/urls.py
+++ quoter/users/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('search/', views.search, name='search'),
+    path('edit/', views.edit_profile, name='edit'),
     path('<slug:username>/', views.profile, name='profile'),
-    path('<slug:username>/followers', views.followers, name='followers'),
-    path('<slug:username>/following', views.following, name='followers'),
 ]
--- quoter/users/views.py
+++ quoter/users/views.py
@@ -1,10 +1,11 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-from .forms import RegisterForm
+from .forms import RegisterForm, ProfileForm
 from .models import Profile
 
 # Create your views here.
@@ -23,20 +24,34 @@ def register(request):
     return render(request, 'registration/register.html', context={'form': form})
 
 def profile(request, username):
-    user = get_object_or_404(User, username=username)
+    profile_user = get_object_or_404(User, username=username)
+    can_edit = request.user.is_authenticated and (request.user.username == username)
 
     try:
-        profile = Profile.objects.get(user=user)
+        profile = Profile.objects.get(user=profile_user)
     except Profile.DoesNotExist:
         profile = Profile()
 
-    return render(request, 'users/profile.html', context={'user': user, 'profile': Profile.Meta.get_fields()})
+    return render(request, 'users/profile.html', context={'can_edit': can_edit, 'profile_username': profile_user.username,'info': profile.get_fields})
 
-def followers(request, username):
-    return render(request, 'users/followers.html', context={'username': username})
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            user_profile = form.save(commit=False)
+            user_profile.user = request.user
+            user_profile.save()
+            
+            username = request.user.username
+            messages.success(request, "Account created for %s" % username)
+            
+            return redirect('users:profile', username=username)
 
-def following(request, username):
-    return render(request, 'users/following.html', context={'username': username})
+    else:
+        form = ProfileForm()
+
+    return render(request, 'users/edit_profile.html', context={'form': form})
 
 def search(request):
     return HttpResponse("coming underway...")
