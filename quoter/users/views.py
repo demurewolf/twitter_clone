6ced70e23a1dae18ffa4545357b95c60af50e701
demurewolf <josiahwedgwood@ymail.com>
2020-12-02 23:57:02 -0500

    Fixed redirect after registering new user

--- quoter/users/views.py
+++ quoter/users/views.py
@@ -16,7 +16,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, "Account created for %s" % username)
-            return redirect('registration/login/')
+            return redirect('login')
 
     else:
         form = RegisterForm()
